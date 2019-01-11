e0a87abb8fa1ed1899d7ee5aadd13bc9e5651ec1
Nguyen Ba Hung <nguyen.ba.hung@framgia.com>
2019-01-11 16:01:35 +0700

    update styled for frontend

---
+++ frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sell extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is Sell page!</p>
+      </div>
+    )
+  }
+}
